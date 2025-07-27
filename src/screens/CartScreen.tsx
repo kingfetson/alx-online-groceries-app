1621ad2299396cb771660fd216cf7c6e49c99826
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-07-27 21:28:53 +0300

    Add bottom tab navigation with styled Home and Profile screens

---
+++ src/screens/CartScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/navigation';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Cart'>;
+
+export default function CartScreen({ navigation, route }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text>Cart Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/screens/ProductScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/navigation';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Product'>;
+
+export default function ProductScreen({ navigation, route }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text>Product Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
