a18569eedb342922ef0bbd871f93bfc4d8e49bc4
kingfetson <171272139+kingfetson@users.noreply.github.com>
2025-07-29 17:18:18 +0300

    fixed: some bugs in files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
+    "./index.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2A9D8F",
+        secondary: "#E9C46A",
+        accent: "#F4A261",
+        danger: "#E76F51",
+      },
+    },
   },
   plugins: [],
-}
+};
